e20ddea64da9b39fd0c82348a41a97d6afd663a7
luisfelipekai <luisfelipekai@gmail.com>
2016-09-21 14:05:38 -0300

    Query string whitspace problem SOLVED

--- popup.js
+++ popup.js
@@ -4,7 +4,9 @@ document.addEventListener('DOMContentLoaded', function(){
 		chrome.tabs.getSelected(null, function(tab){
 				var i = document.createElement("img");
 				var url = 'http://127.0.0.1:8000/DanbooruAPI/downloadImagesUrl/?url='+tab.url;
-				var url_clean = url.replace(/&/gi, '%26')
+				//var url_clean = url.replace(/&/gi, '%26');
+				var url_clean = url.replace(/&/gi, '%26').replace(/\+/g, "%2B");
+				//var second_url_clean = url_clean.replace("/\+"gi, '%2B');
 				i.src = url_clean
 				alert(i.src);	
 		});
